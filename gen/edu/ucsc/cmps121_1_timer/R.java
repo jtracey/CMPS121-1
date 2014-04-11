fc90b0e577ab5071f266dc6f44bbf228dc00d4f6
Justin Tracey <j2tracey@gmail.com>
2014-04-10 18:55:07 -0700

    Remove references to fragment, attempt to fix rotation bug again.

--- gen/edu/ucsc/cmps121_1_timer/R.java
+++ gen/edu/ucsc/cmps121_1_timer/R.java
@@ -1095,10 +1095,10 @@ containing a value of this type.
         public static final int activity_chooser_view_content=0x7f060025;
         public static final int always=0x7f06000b;
         public static final int beginning=0x7f060011;
-        public static final int button1=0x7f06003f;
-        public static final int button2=0x7f060041;
+        public static final int button1=0x7f060041;
+        public static final int button2=0x7f06003f;
         public static final int button3=0x7f06003e;
-        public static final int button4=0x7f060040;
+        public static final int button4=0x7f06003d;
         public static final int checkbox=0x7f06002d;
         public static final int collapseActionView=0x7f06000d;
         public static final int container=0x7f06003c;
@@ -1141,7 +1141,7 @@ containing a value of this type.
         public static final int split_action_bar=0x7f06001e;
         public static final int submit_area=0x7f060039;
         public static final int tabMode=0x7f060002;
-        public static final int textView1=0x7f06003d;
+        public static final int textView1=0x7f060040;
         public static final int title=0x7f06002b;
         public static final int top_action_bar=0x7f060020;
         public static final int up=0x7f060021;
@@ -1199,8 +1199,7 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_main=0x7f030018;
-        public static final int fragment_main=0x7f030019;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
+        public static final int support_simple_spinner_dropdown_item=0x7f030019;
     }
     public static final class menu {
         public static final int main=0x7f0d0000;
