2a6b1c0ca5925f43c9a642fcb6b8cb7579c561f4
Justin Tracey <j2tracey@gmail.com>
2014-04-10 17:52:53 -0700

    Add support for sound, minor bugfixes, minor refactoring.

--- src/edu/ucsc/cmps121_1_timer/MainActivity.java
+++ src/edu/ucsc/cmps121_1_timer/MainActivity.java
@@ -1,8 +1,9 @@
 package edu.ucsc.cmps121_1_timer;
 
-import android.support.v7.app.ActionBarActivity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,27 +11,29 @@ import android.view.WindowManager;
 import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
-	
 	private long counter;
 	private CountDownTimer timer;
-	
+	private MediaPlayer beep;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
         counter = 0;
         timer = null;
+        beep = MediaPlayer.create(getApplicationContext(), R.raw.alarmclock_mechanical_trim);
 
         // Prevents the screen from dimming and going to sleep.
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
     }
-    
+
 	@Override
 	protected void onResume() {
 		super.onResume();
 		counter = 0;
-		timer = null;		
+		timer = null;
+		beep = MediaPlayer.create(getApplicationContext(), R.raw.alarmclock_mechanical_trim);
 	}
 
 
@@ -54,6 +57,18 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
     
+    @Override
+    public void onPause() {
+    	beep.release();
+    	super.onPause();
+    }
+    
+    @Override
+    public void onStop() {
+    	beep.release();
+    	super.onStop();
+    }
+
 	public void clickStart(View v) {
 		if (timer == null)
 			startCounter();
@@ -61,26 +76,35 @@ public class MainActivity extends ActionBarActivity {
 			timer.cancel();
 			timer = null;
 		}
+		
+		if (beep.isPlaying())
+			beep.pause();
 	}
-	
+
 	public void clickPlus(View v) {
 		counter += 60 * 1000;
 		if (timer == null)
 			displayCount();
 		else startCounter();
 	}
-	
+
 	public void clickMinus(View v) {
 		counter -= 60 * 1000;
 		counter = Math.max(0, counter);
 		if (timer == null)
 			displayCount();
 		else if (counter == 0)
-			timer.onFinish();
+			clearTimer();
 		else startCounter();
 	}
-	
+
 	public void clickReset(View v) {
+		clearTimer();
+		if (beep.isPlaying())
+			beep.pause();
+	}
+
+	private void clearTimer() {
 		counter = 0;
 		if (timer != null) {
 			timer.cancel();
@@ -88,31 +112,32 @@ public class MainActivity extends ActionBarActivity {
 		}
 		displayCount();
 	}
-	
 
-private void startCounter() {
-    if (timer != null) {
-        timer.cancel();
-    }
-    displayCount();
-    if (counter > 0) {
-        timer = new CountDownTimer(counter, 1000) {
-                @Override
-				public void onTick(long remainingTimeMillis) {
-                    counter = remainingTimeMillis;
-                    displayCount();
-                }
-                @Override
-				public void onFinish() {
-                    counter = 0;
-                    displayCount();
-                    this.cancel();
-                    timer=null;
-                }
-            };
-        timer.start();
-    }
-}
+	private void startCounter() {
+	    if (timer != null) {
+	        timer.cancel();
+	    }
+	    displayCount();
+	    if (counter > 0) {
+	    	// 100ms is far too often for a single-second precision timer,
+	    	// but the way the timer class works makes the timer lag by the
+	    	// given amount every time the + or - buttons are pressed.
+	        timer = new CountDownTimer(counter, 100) {
+	                @Override
+					public void onTick(long remainingTimeMillis) {
+	                    counter = remainingTimeMillis;
+	                    displayCount();
+	                }
+	                @Override
+					public void onFinish() {
+	                    clearTimer();
+	                    beep.seekTo(0);
+	                    beep.start();
+	                }
+	            };
+	        timer.start();
+	    }
+	}
 
 
 	private void displayCount() {
